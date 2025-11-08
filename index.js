92dec6c6daf4ee2194b39319fa345cda0e660f8e
ujjal-web <ujjaldas827@gmail.com>
2025-11-09 00:20:59 +0600

    server running with test route by updating index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("FinEase Server is running");
+});
+
+
+app.listen(port, () => {
+    console.log(`FinEase Server is running on port ${port}`);
+});
