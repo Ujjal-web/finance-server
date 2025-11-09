ddce0d3f960647efdb8362926c7e4e5306bf7186
ujjal-web <ujjaldas827@gmail.com>
2025-11-09 23:56:59 +0600

    token verification, post and get added

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const admin = require("firebase-admin");
-
-require("dotenv").config()
+require("dotenv").config();
 
 const serviceAccount = require("./serviceKey.json");
 
 const app = express();
 const port = 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -18,57 +16,88 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
 
+// MongoDB connection
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.iyiq1ye.mongodb.net/?appName=Cluster0`;
 
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
 });
 
 const verifyToken = async (req, res, next) => {
     const authorization = req.headers.authorization;
-
     if (!authorization) {
         return res.status(401).send({ message: "Unauthorized: Token not found!" });
     }
-
     const token = authorization.split(" ")[1];
     try {
         await admin.auth().verifyIdToken(token);
-        next(); // Allow access to protected route
+        next();
     } catch (error) {
         console.error("Token verification failed:", error.message);
         return res.status(401).send({ message: "Unauthorized: Invalid token." });
     }
 };
 
+
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
+        const db = client.db("finEaseDB");
+        const transactionsCollection = db.collection("transactions");
+
+        app.post("/transactions", async (req, res) => {
+            const data = req.body;
+            if (!data.userEmail) {
+                return res
+                    .status(400)
+                    .send({ success: false, message: "Missing userEmail field!" });
+            }
+
+            const result = await transactionsCollection.insertOne(data);
+            res.send({
+                success: true,
+                insertedId: result.insertedId,
+            });
+        });
+
+        app.get("/transactions", async (req, res) => {
+            const { email } = req.query;
+            let query = {};
+            if (email) query = { userEmail: email };
+
+            const transactions = await transactionsCollection
+                .find(query)
+                .sort({ createdAt: -1 })
+                .toArray();
+
+            res.send({
+                success: true,
+                count: transactions.length,
+                transactions,
+            });
+        });
+
         await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("MongoDB Connected Successfully!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+
     }
 }
 run().catch(console.dir);
 
-
-
-
 app.get("/", (req, res) => {
-    res.send("FinEase Server is running with mongodb");
+    res.send("FinEase Server is running with MongoDB");
+});
+
+app.get("/secure", verifyToken, (req, res) => {
+    res.send("Access granted! Token verified successfully.");
 });
 
 
 app.listen(port, () => {
-    console.log(`FinEase Server is running on port ${port}`);
+    console.log(`FinEase Server running on http://localhost:${port}`);
 });
